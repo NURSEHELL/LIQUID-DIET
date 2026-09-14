b2e700f8d1fa207353c8b00dcf1aa456142b0510
Adam <endernatie@gmail.com>
2026-09-13 22:23:31 -0300

    Pre-Alpha 1.1
    Added items. Inventory still unfinished, planning to make it so it has 3 slots instead of only 1.

--- LIQUID DIET/liquid.js
+++ LIQUID DIET/liquid.js
@@ -1,141 +1,148 @@
 // Enemy array. "NAME", "Images/img_src", HP, [Attack Pattern (6 turns), Item Drop]
 const Enemies = [
-    ["PATHETIC DUMMY", "Images/Enemy_PATHETIC_DUMMY.png", 3, [1, 0, 1, 0, 1, 0], 1 ],
-    ["PITIFUL DUMMY", "Images/Enemy_PATHETIC_DUMMY.png", 3, [0, 1, 0, 1, 0, 1], 2 ],
+    ["PATHETIC DUMMY", "Images/Enemy_PATHETIC_DUMMY.png", 3, [1, 0, 1, 0, 1, 0], 1],
+    ["PITIFUL DUMMY", "Images/Enemy_PATHETIC_DUMMY.png", 3, [0, 1, 0, 1, 0, 1], 2],
 ]
 // 0 = Wait / 1 = Attack 1HP
 
 // Item array. "NAME", "Images/img_src", Utility, Utility Specifics]
 const Items = [
-    ["Empty", "Images/Item_Empty.png", itemNull, null],
+    ["Empty", "Images/Item_Empty.png", itemNull],
     ["Energy Drink", "Images/Item_Energy.png", itemHeal, 1],
     ["Sludge", "Images/Item_Sludge.png", itemDMG, 2],
 ]
 
-// Item Utility
-function itemNull() {}
 
-    function itemExhaust() {
-        document.getElementById("itemSlot").addEventListener("click", Items[0][2]);
-        itemChoose = Items[0];
-    }
+itemSlot1 = Items[0]
+enemyItemDrop = Items[0]
+currentPlayerHP = 3
 
 function itemHeal() {
-    currentPlayerHP += Items[Enemies[randomEnemy][4]][3]
+    currentPlayerHP += itemSlot1[3]
     document.getElementById("playerHP").innerHTML = currentPlayerHP;
-
-itemExhaust();
+    useItem()
 }
 
 function itemDMG() {
-    currentEnemyHP -= Items[Enemies[randomEnemy][4]][3]
+    currentEnemyHP -= itemSlot1[3]
     document.getElementById("enemyHP").innerHTML = currentEnemyHP;
-
-itemExhaust();
+    useItem()
 }
 
-//
-
-// Discard Item
-
-function itemDiscard() {
-itemExhaust();
-randomizeEnemy();
+function itemNull() {
+    document.getElementById("actionLog").innerHTML += "There is no item in this slot... <br>";
 }
 
-var currentPlayerHP = 3;
-document.getElementById("playerHP").innerHTML = currentPlayerHP;
-discardItem.disabled = true;
+function useItem() {
+        itemSlot1 = Items[0];
+        document.getElementById("inventory1").innerHTML = itemSlot1[0];
+        document.getElementById("actionLog").innerHTML += "You used your thingy <br>";
+}
 
-// ON LOAD
-itemChoose = Items[0];
-document.getElementById("itemSlot").addEventListener ("click", pickItem);
-document.getElementById("itemSlot").addEventListener ("click", useItem);
-window.onload = function() {
-randomizeEnemy();
+// ON PAGE LOAD
+window.onload = function () {
+    document.getElementById("playerHP").innerHTML = currentPlayerHP;
+    document.getElementById("inventory1").innerHTML = itemSlot1[0];
+    randomizeEnemy();
+
 };
 
+// RANDOM ENEMY
+
 function randomizeEnemy() {
+    elapsedTurns = 0;
     playerAttack.disabled = false;
-    discardItem.disabled = true;
+
+    // Clear log
     document.getElementById("actionLog").innerHTML = " ";
-elapsedTurns = 0;
 
-// Randomize an enemy
-randomEnemy = Math.floor(Math.random() * Enemies.length);
+    // Randomize
+    randomEnemy = Math.floor(Math.random() * Enemies.length);
 
-// Set enemy info
-currentEnemyHP = Enemies[randomEnemy][2];
-enemyName = Enemies[randomEnemy][0];
-
-
+    // Set enemy info
+    currentEnemyHP = Enemies[randomEnemy][2];
+    enemyName = Enemies[randomEnemy][0];
     document.getElementById("enemyHP").innerHTML = currentEnemyHP;
     document.getElementById("enemyName").innerHTML = enemyName;
     document.getElementById("enemyImg").src = Enemies[randomEnemy][1];
 }
 //
 
-    // ENEMY TURN
+currentPlayerHP = 3;
+
+// ENEMY TURN
+
 function enemyTurn() {
-discardItem.disabled = true
 
-// IF enemy waits (0)
-if (Enemies[randomEnemy][3][elapsedTurns] == 0) {
-document.getElementById("actionLog").innerHTML += "The enemy is waiting... <br>";
-}
+    // IF enemy waits (0)
+    if (Enemies[randomEnemy][3][elapsedTurns] == 0) {
+        document.getElementById("actionLog").innerHTML += "The enemy is waiting... <br>";
+    }
 
-// IF enemy attacks (1)
-if (Enemies[randomEnemy][3][elapsedTurns] == 1) {
-document.getElementById("actionLog").innerHTML += "The enemy attacks! <br>";
-currentPlayerHP -= 1;
-}
+    // IF enemy attacks (1)
+    if (Enemies[randomEnemy][3][elapsedTurns] == 1) {
+        document.getElementById("actionLog").innerHTML += "The enemy attacks! <br>";
+        currentPlayerHP -= 1;
+    }
 
-document.getElementById("playerHP").innerHTML = currentPlayerHP;
-playerAttack.disabled = false;
+    if (Enemies[randomEnemy][3][elapsedTurns] == 2) {
+        document.getElementById("actionLog").innerHTML += "The enemy heals itself! <br>";
+        currentPlayerHP += 2;
+    }
+
+    document.getElementById("playerHP").innerHTML = currentPlayerHP;
+    playerAttack.disabled = false;
 
 
 }
 
 // Enemy Defeat
 function enemyDefeat() {
-document.getElementById("actionLog").innerHTML += "Enemy defeated! You win! <br>";
-document.getElementById("enemyImg").src = "Images/PLACEHOLDER.png";
-itemDrop();
+    enemyItemDrop = Items[Enemies[randomEnemy][4]];
+
+if (itemSlot1 = Items[0]) {
+        document.getElementById("inventory1").innerHTML = enemyItemDrop[0];
+        document.getElementById("inventory1").addEventListener("click", enemyItemDrop[2], { once: true });
+        itemSlot1 = enemyItemDrop;
 }
 
-function itemDrop() {
-itemChoose = Items[Enemies[randomEnemy][4]];
-document.getElementById("actionLog").innerHTML += "ITEM GOT! " + itemChoose[0] + "<br>";
-document.getElementById("actionLog").innerHTML += "Choose an item to replace. <br>";
-discardItem.disabled = false;
+    document.getElementById("actionLog").innerHTML += "Enemy defeated! You win! <br>";
+    document.getElementById("enemyImg").src = "Images/Enemy_DEFEATED.png";
+    document.getElementById("actionLog").innerHTML += "ITEM GOT! " + enemyItemDrop[0] + "<br>";
+
+    // Next Enemy
+    setTimeout(() => { randomizeEnemy() }, 2500);
 }
 
+// Item Systems
 
-// Player Attack
+//
+
+// PLAYER ATTACK
 
 function Attack() {
-playerAttack.disabled = true
+    playerAttack.disabled = true
 
 
-currentEnemyHP -= 1;
-document.getElementById("actionLog").innerHTML += "You attack the enemy! <br>";
-document.getElementById("enemyHP").innerHTML = currentEnemyHP;
-    
-setTimeout(() => {
+    currentEnemyHP -= 1;
+    document.getElementById("actionLog").innerHTML += "You attack the enemy! <br>";
+    document.getElementById("enemyHP").innerHTML = currentEnemyHP;
 
-elapsedTurns += 1;
-if (elapsedTurns >= 6) {
-    elapsedTurns = 0
-}
+    setTimeout(() => {
 
-if (currentEnemyHP >= 1) {
-  enemyTurn();
- }
+        elapsedTurns += 1;
+        if (elapsedTurns >= 6) {
+            elapsedTurns = 0
+        }
 
-  else {
-  enemyDefeat();
-  };
-  
-}, 1000);
+        if (currentEnemyHP >= 1) {
+            enemyTurn();
+        }
+
+        else {
+            enemyDefeat();
+        };
+
+    }, 1000);
 
 }
